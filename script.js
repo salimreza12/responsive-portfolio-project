1b6a993a4b3253121a94f1701b4df4782a62ec0a
Salim Reza <82095538+salimreza12@users.noreply.github.com>
2022-06-21 16:55:20 +0600

    add some html and others file

---
+++ script.js
@@ -0,0 +1,13 @@
+// typing script js
+var typed = new Typed(".typing", {
+  strings: ["Web Deginer", "Web Developer", "Youtuber", "Blogger"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+var typed = new Typed(".typing1", {
+  strings: ["Web Deginer", "Web Developer", "Youtuber", "Blogger"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
